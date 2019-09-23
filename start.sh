c82ef92cc1888eda274b71ba30c0a9eb29e066da
lxthyme <1094426094@qq.com>
2019-09-24 01:04:55 +0800

    initial commit

--- start.sh
+++ start.sh
@@ -10,8 +10,9 @@ npm install -g react-native-cli
 # set https_proxy=http://127.0.0.1:1080
 
 # set http_proxy=http://127.0.0.1:1087, set https_proxy=http://127.0.0.1:1087
+# set http_proxy=http://127.0.0.1:1080, set https_proxy=http://127.0.0.1:1080
 # set HTTP_PROXY=http://127.0.0.1:1087
-netsh winhttp set proxy "http://127.0.0.1:1080" bypass-list="google.com"
-netsh winhttp set proxy "http://127.0.0.1:1080/pac?auth=jcga4w-0Xg5tQXRbVm3Z&t=201909232150199822"
-netsh winhttp show proxy
+# netsh winhttp set proxy "http://127.0.0.1:1080" bypass-list="google.com"
+# netsh winhttp set proxy "http://127.0.0.1:1080/pac?auth=jcga4w-0Xg5tQXRbVm3Z&t=201909232150199822"
+# netsh winhttp show proxy
 # npm config set proxy=http://127.0.0.1:1080
